13cd36370337e61de7b34b4f2fec5bdd6cb6c535
Sean <sean1981github@users.noreply.github.com>
2020-06-04 21:14:24 +0800

    [Sean] fixed cypress and environment issues

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,8 +12,6 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const dotenvPlugin = require("cypress-dotenv");
-
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -21,6 +19,4 @@ const dotenvPlugin = require("cypress-dotenv");
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  config = dotenvPlugin(config);
-  return config;
 };
