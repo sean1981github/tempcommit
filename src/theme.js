947f7e4af4b64c9e4e24f919cdd64a2a70949ad5
Sean <sean1981github@users.noreply.github.com>
2020-06-05 19:28:07 +0800

    [Sean/Yan Chao] #32 remove code smell

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,6 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { orange } from "@material-ui/core/colors/";
 
-// const overrides = {
-//   MuiTab: {
-//     root: {
-//       backgroundColor: "#336699",
-//       "&$selected": {
-//         backgroundColor: "#4080bf",
-//       },
-//     },
-//   },
-// };
-
 export default createMuiTheme({
   palette: {
     primary: {
