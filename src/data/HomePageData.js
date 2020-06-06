10ebd1df7dde5828b128e67405cde14c8a5279a5
Yanchao Du <lothdudu@gmail.com>
2020-06-07 00:57:14 +0800

    [yanchao/sean]#32 fix minor issue in .gitignore so data for setting up homepage can be pushed

---
+++ src/data/HomePageData.js
@@ -0,0 +1,66 @@
+const HomePageData = [
+  {
+    role: "QM",
+    cards: [
+      {
+        title: "CATEGORY",
+        buttons: [
+          { title: "Create New Category", path: "" },
+          { title: "Edit Existing Category", path: "" },
+        ],
+      },
+      {
+        title: "PROBLEM SET",
+        buttons: [
+          { title: "Create New Set", path: "" },
+          { title: "Edit Existing Set", path: "" },
+        ],
+      },
+      {
+        title: "PROBLEM",
+        buttons: [
+          { title: "Create New Problem", path: "/problem/add" },
+          { title: "Edit Existing Problem", path: "" },
+        ],
+      },
+      {
+        title: "QUIZ TEMPLATE",
+        buttons: [
+          { title: "Create New Template", path: "" },
+          { title: "Edit Existing Template", path: "" },
+        ],
+      },
+    ],
+  },
+  {
+    role: "HR",
+    cards: [
+      {
+        title: "CREATE QUIZ",
+        buttons: [
+          { title: "Create New Quiz", path: "" },
+          { title: "Delete Existing Quiz", path: "" },
+        ],
+      },
+      {
+        title: "QUIZ RESULTS",
+        buttons: [{ title: "View Results", path: "" }],
+      },
+    ],
+  },
+  {
+    role: "ASSESSOR",
+    cards: [
+      {
+        title: "RESULTS",
+        buttons: [{ title: "View Results", path: "" }],
+      },
+    ],
+  },
+  {
+    role: "ADMIN",
+    cards: [],
+  },
+];
+
+export default HomePageData;
