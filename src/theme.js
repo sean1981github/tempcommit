e36745ef3551c2a1433cd49a35508dbfb36ae51c
Sean <sean1981github@users.noreply.github.com>
2020-06-07 11:36:38 +0800

    [Sean/Yan Chao] #32 removed duplicate code in theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,15 +2,6 @@ import { createMuiTheme } from "@material-ui/core/styles";
 import { orange } from "@material-ui/core/colors/";
 
 export default createMuiTheme({
-  MuiTab: {
-    root: {
-      backgroundColor: "#336699",
-      "&$selected": {
-        backgroundColor: "#4080bf",
-      },
-    },
-  },
-
   palette: {
     primary: {
       main: "#336699",
