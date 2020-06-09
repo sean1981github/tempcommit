a388924fdc529011a3abfcd5c64fd4e1ebfdce9a
raychua <raychua@users.noreply.github.com>
2020-06-09 23:34:29 +0800

    [ray] remove router.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "@testing-library/jest-dom/extend-expect";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("App, to test Router file", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  });
 });
